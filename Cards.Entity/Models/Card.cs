2e7ab13f18303e857573fcb89ddeda0d547ce84c
AlexanderKarcheuski <karchevskji@gmail.com>
2022-03-14 10:27:08 +0300

    Created models

---
+++ Cards.Entity/Models/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards.Entity.Models
+{
+    public class Card
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+
+        public Card()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
